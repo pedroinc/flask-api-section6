9005055a5e12df8ad18e3592d0a21cd447635e74
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2018-02-28 16:15:14 -0300

    iteration with lambda

--- code/app.py
+++ code/app.py
@@ -8,12 +8,15 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None }, 404
+        # lambda function
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': None }, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            #bad request - status code 400
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
