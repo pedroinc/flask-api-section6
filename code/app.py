1e3a0111d8856782ca6ced66046fd0205b33ca67
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2018-03-01 14:45:47 -0300

    put method added

--- code/app.py
+++ code/app.py
@@ -14,7 +14,7 @@ items = []
 
 class Item(Resource):
 
-    @jwt_required()
+    # @jwt_required()
     def get(self, name):
         # lambda function
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -36,6 +36,18 @@ class Item(Resource):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message', 'Item deleted'}
+
+    #create or update existing items
+    # @jwt_required()
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price'] }
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
