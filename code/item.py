93cebb5e9819d9265f1bcecff59c6f23f6fbb560
Pedro Soares <pedro.soares.lacerda@tvglobo.com.br>
2018-03-02 19:32:34 -0300

    save, delete and list_all items from database - check

--- code/item.py
+++ code/item.py
@@ -4,6 +4,9 @@ from flask_jwt import jwt_required
 
 
 class Item(Resource):
+
+    database_file = 'my_database.db'
+
     parser = reqparse.RequestParser()
     parser.add_argument('price',
         type=float,
@@ -13,36 +16,58 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
-        connection = sqlite3.connect('my_database.db')
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect(Item.database_file)
         cursor = connection.cursor()
 
         query = "SELECT * FROM items WHERE name = ?"
         result = cursor.execute(query, (name,))
         row = result.fetchone()
-
         connection.close()
 
         if row:
             return {'item': {'name': row[0], 'price': row[1] } }, 200
 
-        return {'message': 'Item not found'}, 404
-
     # @jwt_required()
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        if self.find_by_name(name):
             return {'message': "An item with name '{}' already exists".format(name)}, 400 #bad req status code
 
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
-        items.append(item)
+
+        connection = sqlite3.connect(Item.database_file)
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
 
     # @jwt_required()
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message', 'Item deleted'}
+        if not self.find_by_name(name):
+            return {'message': "Item not found"}, 404 #not found
+
+        connection = sqlite3.connect(Item.database_file)
+        cursor = connection.cursor()
+        query = "DELETE FROM items WHERE name = ?"
+        cursor.execute(query, (name,))
+
+        connection.commit()
+        connection.close()
+
+        return {'message': 'Item deleted'}
 
     #create or update existing items
     # @jwt_required()
@@ -59,4 +84,16 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        return {'items': items}
+        connection = sqlite3.connect(Item.database_file)
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+        rows = result.fetchall()
+        connection.close()
+
+        items = []
+        for row in rows:
+            items.append({'item': {'name': row[0], 'price': row[1] } })
+
+        return {'items': items}, 200
