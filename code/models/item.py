1b052aa17d46272bf8de05f794d41cfc9c1ec455
Pedro Soares Lacerda <pedro.soares.lacerda@tvglobo.com.br>
2018-03-12 16:51:49 -0300

    refactoring item -> find_by_name method

--- code/models/item.py
+++ code/models/item.py
@@ -18,16 +18,8 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect(Utils.DATABASE_FILE)
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name = ?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return cls(*row)
+        return cls.query.filter_by(name=name).first()
+        # return ItemModel.query.filter_by(name=name).first()
 
     def insert(self):
         connection = sqlite3.connect(Utils.DATABASE_FILE)
