f848368ec58361db79f186bc26937c2b7e805058
Pedro Soares <pedroinc@gmail.com>
2018-03-22 19:45:39 -0300

    changes to make it possible to deploy on heroku

--- app.py
+++ app.py
@@ -6,16 +6,13 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.common import Utils
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_database.db'
 app.secret_key = 'dontask'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 # creates a new endpoint: /auth
 jwt = JWT(app, authenticate, identity)
@@ -27,6 +24,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == "__main__":
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
---
+++ code/README.md
@@ -0,0 +1,2 @@
+# flask-api-section6
+Udemy flask api course - section 6
